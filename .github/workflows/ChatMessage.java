6df9c999b8bd6df917940c4bec59cd5181ddfa19
shriharshk <34976568+shriharshk@users.noreply.github.com>
2025-07-15 16:38:40 +0530

    Create ChatMessage.java

---
+++ .github/workflows/ChatMessage.java
@@ -0,0 +1,17 @@
+public class ChatMessage {
+    private String message;
+    private boolean isUser; // true if message sent by user
+
+    public ChatMessage(String message, boolean isUser) {
+        this.message = message;
+        this.isUser = isUser;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isUser() {
+        return isUser;
+    }
+}
