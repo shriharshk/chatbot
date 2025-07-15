32d2ad3626fdb9583739ce8f0ccb8426ee1874b7
shriharshk <34976568+shriharshk@users.noreply.github.com>
2025-07-15 16:39:02 +0530

    Create ChatAdapter.java

---
+++ .github/workflows/ChatAdapter.java
@@ -0,0 +1,46 @@
+public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ChatViewHolder> {
+
+    private List<ChatMessage> chatMessages;
+
+    public ChatAdapter(List<ChatMessage> chatMessages) {
+        this.chatMessages = chatMessages;
+    }
+
+    @NonNull
+    @Override
+    public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view;
+        if (viewType == 0) { // user message
+            view = LayoutInflater.from(parent.getContext())
+                  .inflate(R.layout.user_message, parent, false);
+        } else { // chatbot message
+            view = LayoutInflater.from(parent.getContext())
+                  .inflate(R.layout.bot_message, parent, false);
+        }
+        return new ChatViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ChatViewHolder holder, int position) {
+        holder.message.setText(chatMessages.get(position).getMessage());
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return chatMessages.get(position).isUser() ? 0 : 1;
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    public class ChatViewHolder extends RecyclerView.ViewHolder {
+        TextView message;
+
+        public ChatViewHolder(@NonNull View itemView) {
+            super(itemView);
+            message = itemView.findViewById(R.id.messageText);
+        }
+    }
+}
